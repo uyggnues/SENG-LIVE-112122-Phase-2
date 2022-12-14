730104c0adb2936dbef5903d970e1d35ee541306
Louis <Louis.Medina@flatironschool.com>
2022-12-14 13:22:58 -0500

    update starter code for 03 lecture

--- 03_information_flow/project_showcase/src/components/Header.js
+++ 03_information_flow/project_showcase/src/components/Header.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 
 const Header = () => {
@@ -7,6 +6,20 @@ const Header = () => {
   const handleClick = () => setIsDarkMode(!isDarkMode);
 
   const buttonTextContent = isDarkMode ? "Light Mode" : "Dark Mode";
+
+  // Deliverable 2: Use Inverse Data flow to implement 
+  // Light-Dark mode
+
+  // - Refact isDarkMode state from the `Header` 
+  // component to the `App` component.
+
+  // - Create a callback function that updates 
+  // `isDarkMode` and pass the callback function 
+  // as a prop to the `Header` component
+
+  // - Inside the `Header` component, invoke the 
+  // callback function in place of updating the 
+  // state
 
   return (
     <header>
--- 03_information_flow/project_showcase/src/components/ProjectList.js
+++ 03_information_flow/project_showcase/src/components/ProjectList.js
@@ -14,6 +14,28 @@ const ProjectList = ({ projects }) => {
 
   const handleOnChange = (e) => setSearchQuery(e.target.value);
 
+  // Deliverable 3: Refactor the filter component out of 
+  // `ProjectList` and implement inverse data flow
+
+    // - Refactor the `searchQuery` state and the filter 
+    // method inside of the `ProjectList` component to 
+    // the `App` component
+  
+    // - Using inverse data flow, get the value of the 
+    // input field UP to the App component
+  
+    // - Write a callback function inside the App 
+    // component:
+  
+      //   - the function should take in a new search value 
+      // and set state with that value
+      
+      //   - pass the callback function down as a prop to 
+      // `ProjectList`
+  
+    // - Call the callback function from the onChange 
+    // event listener
+
   return (
     <section>
       <h2>Projects</h2>
