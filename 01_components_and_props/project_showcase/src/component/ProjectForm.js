48f3cf71f632d5e4a7ea08553b68b8346704b61e
Seunggyu <seunggyul14@icloud.com>
2022-12-13 19:00:51 -0800

    boo

--- 01_components_and_props/project_showcase/src/component/ProjectForm.js
+++ 01_components_and_props/project_showcase/src/component/ProjectForm.js
@@ -1,7 +1,16 @@
-const ProjectForm =() => {
+const ProjectForm = () => {
     return (
-        <h2>ProjectForm Component</h2>
+      <section>
+        <form className="form">
+          <h3>Add New Project</h3>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" name="name" />
+          <label htmlFor="about">About</label>
+          <textarea id="about" name="about" />
+          <button type="submit">Add Project</button>
+        </form>
+      </section>
     );
-}
-
-export default ProjectForm;
+  };
+  
+  export default ProjectForm;
