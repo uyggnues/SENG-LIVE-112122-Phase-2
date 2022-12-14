918106792034f06d153bc23de96234f901f181ec
Seunggyu <seunggyul14@icloud.com>
2022-12-13 18:16:52 -0800

    working

--- 01_components_and_props/project_showcase/src/component/ProjectListItem.js
+++ 01_components_and_props/project_showcase/src/component/ProjectListItem.js
@@ -1,10 +1,36 @@
 // should render once for every object inside "projects"
+import { useState } from "react"
+const ProjectListItem = ({id, about, image, link, name, phase}) => {
 
-const ProjectListItem = ({project}) => {
+    const [clapCount, setClapCount] = useState(0)
+
+    const handleClaps = (e) => {
+        console.log(e.target);
+        setClapCount(clapCount + 1);
+    }
 
     return (
-        <p>{project.name}</p>
+        <li className="card">
+            <figure className="image">
+                <image src={image} alt={name} />
+                <button onClick={handleClaps} className="claps">👏{clapCount}</button>
+            </figure>
+
+            <section className="details">
+                <h4>{name}</h4>
+                <p>{about}</p>
+                {link ? (
+                    <p>
+                        <a href={link}>link</a>
+                    </p>
+                ) : null}
+            </section>
+
+            <footer className="extra">
+                <span className="badge blue">Phase {phase}</span>
+            </footer>
+        </li>
     );
-}
+};
 
 export default ProjectListItem;
